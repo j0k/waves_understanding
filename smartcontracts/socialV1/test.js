6bdd7a96f0c5519823a6ade46dacf0409f442ecc
Jo <yuri@tusion.xyz>
2019-05-13 16:11:19 +0300

    version with transfer by invoke

--- smartcontracts/socialV1/test.js
+++ smartcontracts/socialV1/test.js
@@ -1,4 +1,3 @@
-
 A = []
 for (var i in env.accounts){
     seed = env.accounts[i]
@@ -22,12 +21,59 @@ describe('Wallet test suite', () => {
     })
 
     it ('InvokeTx: initOwnership', async function(){
+
 		const params = {
-			dappAddress: A[0]["addr"],
+			dApp: A[0]["addr"],
 			call: {
 				function:"initOwnership",
+				args:[{type:"string", value:"transfer_"}]
+			},
+			fee: 900000
+		}
+        const ttx = invokeScript(params, A[0]["seed"])
+        await broadcast(ttx)
+        await waitForTx(ttx.id)
+    })
+
+    it ('InvokeTx: addTransfer', async function(){
+		const params = {
+			dApp: A[0]["addr"],
+			call: {
+				function:"addUserCall",
+				args:[{type:"string", value:"_transfer_"}, {type:"string", value:A[1]["pub"]}]
+			},
+			fee: 900000
+		}
+        const ttx = invokeScript(params, A[0]["seed"])
+        await broadcast(ttx)
+        await waitForTx(ttx.id)
+    })
+
+
+    it ('InvokeTx: transfer()', async function(){
+		const params = {
+			dApp: A[0]["addr"],
+			call: {
+				function:"transferCall",
+				args:[{type:"string", value: A[3]["addr"]}, {type:"integer", value:10000000}]
+			},
+            //senderPublicKey:A[1]["pub"],
+			fee: 900000
+		}
+        const ttx = invokeScript(params, A[1]["seed"])
+        await broadcast(ttx)
+        await waitForTx(ttx.id)
+    })
+
+    it ('InvokeTx: incNum', async function(){
+
+		const params = {
+			dApp: address(env.accounts[0]),
+			call: {
+				function:"incNum",
 				args:[]
 			},
+
 			fee: 900000
 		}
 
